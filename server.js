5020d7bc6f1267bb761b9dfc80d34d7f2dd3fbbb
superfishal <97133716+superfishal@users.noreply.github.com>
2022-10-26 16:45:01 -0700

    trying to fix express set and render

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
-app.set("view engine", "pug");
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.set("view engine", "pug");
 app.route("/").get((req, res) => {
   res.render(process.cwd() + "/views/pug");
 });
