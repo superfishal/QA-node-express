187ab10cc663d4039ccd8120447a861bd46b97d7
superfishal <97133716+superfishal@users.noreply.github.com>
2022-10-27 12:29:15 -0700

    setup mongodb

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ fccTesting(app); // For fCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -22,21 +23,31 @@ app.use(
     cookie: { secure: false },
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
-app.route("/").get((req, res) => {
-  // Change the response to render the Pug template
-  res.render(process.cwd() + "/views/pug/index", {
-    title: "Hello",
-    message: "Please login",
+
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  app.route("/").get((req, res) => {
+    // Change the response to render the Pug template
+    res.render(process.cwd() + "/views/pug/index", {
+      title: "Hello",
+      message: "Please login",
+    });
   });
-});
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
   });
 });
 app.listen(process.env.PORT || 3000, () => {
